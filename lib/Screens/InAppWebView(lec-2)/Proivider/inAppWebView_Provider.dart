7736db98cdbabf6003bf2b92360ecf3fe125b3a4
Admin <maayush235@gmail.com>
2024-05-23 23:10:50 +0530

    Radhey Radhey

--- lib/Screens/InAppWebView(lec-2)/Proivider/inAppWebView_Provider.dart
+++ lib/Screens/InAppWebView(lec-2)/Proivider/inAppWebView_Provider.dart
@@ -5,7 +5,7 @@ class MainProvider extends ChangeNotifier
   double progress = 0;
   String searchtext = '';
 
-  void onchangeprogress(int progress)
+  void onchange_progress(int progress)
   {
     this.progress = progress/100;
     notifyListeners();
